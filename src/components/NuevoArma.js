277871278fd310a986970bd96d788250ac2dab25
Martiniano <martiniano_olivera@hotmail.com>
2019-11-14 22:58:09 -0300

    Second commit in GitHub.

--- src/components/NuevoArma.js
+++ src/components/NuevoArma.js
@@ -13,7 +13,9 @@ const stateInicial = {
 };
 
 class NuevoArma extends Component {
-  state = { ...stateInicial };
+  state = {
+    ...stateInicial
+  };
 
   handleChange = e => {
     // colocar lo que el usuario escribe en el state
@@ -48,7 +50,9 @@ class NuevoArma extends Component {
     }
 
     // generar objeto cons los datos
-    const nuevoArma = { ...this.state.arma };
+    const nuevoArma = {
+      ...this.state.arma
+    };
     nuevoArma.id = uuid();
 
     // agragar el arma al state de App
@@ -67,18 +71,18 @@ class NuevoArma extends Component {
       <div className="card mt-5 py-5">
         <div className="card-body">
           <h2 className="card-title text-center mb-5">
-            Llene el formulario para detallar qué arma falló y cómo.
-          </h2>
+            Llene el formulario para detallar qué arma falló y cómo.{" "}
+          </h2>{" "}
           {error ? (
             <div className="alert alert-danger mt-2 mb-5 text-center">
-              Todos los campos son obligatorios
+              Todos los campos son obligatorios{" "}
             </div>
-          ) : null}
+          ) : null}{" "}
           <form onSubmit={this.handleSubmit}>
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">
-                Nombre de soldado
-              </label>
+                Nombre de soldado{" "}
+              </label>{" "}
               <div className="col-sm-8 col-lg-10">
                 <input
                   type="text"
@@ -87,29 +91,30 @@ class NuevoArma extends Component {
                   name="soldado"
                   onChange={this.handleChange}
                   value={this.state.arma.soldado}
-                />
-              </div>
+                />{" "}
+              </div>{" "}
             </div>
-
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">
-                Nombre del dueño
-              </label>
+                Rango del soldado.{" "}
+              </label>{" "}
               <div className="col-sm-8 col-lg-10">
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Nombre Dueño"
+                  placeholder="Rango soldado."
                   name="rango"
                   onChange={this.handleChange}
                   value={this.state.arma.rango}
-                />
-              </div>
-            </div>
+                />{" "}
+              </div>{" "}
+            </div>{" "}
             {/*form-group */}
-
             <div className="form-group row">
-              <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
+              <label className="col-sm-4 col-lg-2 col-form-label">
+                {" "}
+                Fecha{" "}
+              </label>{" "}
               <div className="col-sm-8 col-lg-4">
                 <input
                   type="date"
@@ -117,13 +122,12 @@ class NuevoArma extends Component {
                   name="fecha"
                   onChange={this.handleChange}
                   value={this.state.arma.fecha}
-                />
-              </div>
-            </div>
+                />{" "}
+              </div>{" "}
+            </div>{" "}
             {/*form-group */}
-
             <div className="form-group row">
-              <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
+              <label className="col-sm-4 col-lg-2 col-form-label"> Hora </label>{" "}
               <div className="col-sm-8 col-lg-4">
                 <input
                   type="time"
@@ -131,15 +135,14 @@ class NuevoArma extends Component {
                   name="hora"
                   onChange={this.handleChange}
                   value={this.state.arma.hora}
-                />
-              </div>
-            </div>
+                />{" "}
+              </div>{" "}
+            </div>{" "}
             {/*form-group */}
-
             <div className="form-group row">
               <label className="col-sm-4 col-lg-2 col-form-label">
-                Motivo de la falla
-              </label>
+                Motivo de la falla{" "}
+              </label>{" "}
               <div className="col-sm-8 col-lg-10">
                 <textarea
                   type="text"
@@ -148,18 +151,17 @@ class NuevoArma extends Component {
                   name="motivoFalla"
                   onChange={this.handleChange}
                   value={this.state.arma.motivoFalla}
-                />
-              </div>
+                />{" "}
+              </div>{" "}
             </div>
-
             <input
               type="submit"
               className="py-3 mt-2 btn btn-success btn-block"
               value="Agregar nuevo arma"
             />
           </form>{" "}
-          {/*form-group */}
-        </div>
+          {/*form-group */}{" "}
+        </div>{" "}
       </div>
     );
   }
